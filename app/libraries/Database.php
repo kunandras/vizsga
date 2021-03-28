69cc2955f204879edc74bba613a7dd1bb0bb40a1
kunandras <50652890+kunandras@users.noreply.github.com>
2021-03-29 00:26:38 +0200

    Update Database.php

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -6,19 +6,22 @@ class Database
     private static string $dbname = 'forum';
     private static string $dbuser = 'root';
     private static string $dbpass = '';
-    private static $dsn;
+    private static PDO $db_singleton;
     
     /**
      * @throws PDOException
      */
     public static function getInstance()
     {
-        self::$dsn = 'mysql:host=' . self::$dbhost . ';dbname=' . self::$dbname . ';charset=utf8';
-        try {
-            self::$dsn = new PDO(self::$dsn, self::$dbuser, self::$dbpass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        } catch (PDOException $e) {
-            throw new PDOException('Sikertelen adatbázis csatlakozás: ' . $e->getMessage());
+        if (empty(self::$db_singleton)) {
+            try {
+                self::$db_singleton = new PDO('mysql:host=' . self::$dbhost . ';dbname=' . self::$dbname . ';charset=utf8', self::$dbuser, self::$dbpass, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]);
+            } catch (PDOException $e) {
+                throw new PDOException('Sikertelen adatbázis csatlakozás: ' . $e->getMessage());
+            }
         }
-        return self::$dsn;
+        return self::$db_singleton;
     }
 }
